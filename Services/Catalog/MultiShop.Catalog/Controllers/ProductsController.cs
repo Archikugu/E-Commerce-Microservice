22798a5344d3f66d4171ab981b3fdbb365f25a57
engokhangok <engokhangokmail.com>
2025-02-27 17:04:08 +0300

    feat: Completed Category and Product Service integration
    - Implemented Category and Product services
    - Integrated AutoMapper for object mapping
    - Added Product and Category DTOs
    - Developed Catalog APIs for Category and Product
    - Configured Swagger for API documentation
    - Implemented API testing for Product, Category, and Product Image services

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductDtos;
+using MultiShop.Catalog.Services.ProductServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _ProductsService;
+
+    public ProductsController(IProductService ProductsService)
+    {
+        _ProductsService = ProductsService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ProductsList()
+    {
+        var values = await _ProductsService.GetAllProductAsync();
+        return Ok(values);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductsById(string id)
+    {
+        var values = await _ProductsService.GetByIdProductAsync(id);
+        return Ok(values);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateProducts(CreateProductDto createProductsDto)
+    {
+        await _ProductsService.CreateProductAsync(createProductsDto);
+        return Ok("Product Successfully Added");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteProducts(string id)
+    {
+        await _ProductsService.DeleteProductAsync(id);
+        return Ok("Product Successfully Deleted");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateProducts(UpdateProductDto updateProductsDto)
+    {
+        await _ProductsService.UpdateProductAsync(updateProductsDto);
+        return Ok("Product Successfully Updated");
+    }
+}
--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.ProductDetailServices;
+using MultiShop.Catalog.Services.ProductImageServices;
+using MultiShop.Catalog.Services.ProductServices;
+using MultiShop.Catalog.Settings;
+using Scalar.AspNetCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +16,36 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwaggerUI(options =>
+    {
+        // This is the endpoint for the Swagger UI
+        options.SwaggerEndpoint("/openapi/v1.json", "MultiShop Catalog API");
+        options.RoutePrefix = string.Empty; // This will make Swagger UI available at the root (e.g., https://localhost:7212)
+    });
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
