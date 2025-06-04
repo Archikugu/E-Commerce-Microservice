84a94ece7b7d0cf64e6a76ff24cb91dcc48a05d5
Archikugu <engokhangok@gmail.com>
2025-06-04 14:43:31 +0300

    Added JWT configuration

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -1,52 +1,81 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace MultiShop.IdentityServer;
 
 public static class Config
 {
-    public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
+    public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+    {
+        new ApiResource("ResourceCatalog")
         {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Profile(),
-        };
-
-    public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
+            Scopes = { "CatalogFullPermission", "CatalogReadPermission" }
+        },
+        new ApiResource("ResourceDiscount")
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
-        };
-
-    public static IEnumerable<Client> Clients =>
-        new Client[]
+            Scopes = { "DiscountFullPermission" }
+        },
+        new ApiResource("ResourceOrder")
         {
-            // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
+            Scopes = { "OrderFullPermission" }
+        }
+    };
 
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+    public static IEnumerable<IdentityResource> IdentityResources = new IdentityResource[]
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Email(),
+        new IdentityResources.Profile()
+    };
 
-                AllowedScopes = { "scope1" }
+    public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+    {
+        new ApiScope("CatalogFullPermission", "Full access to catalog operations"),
+        new ApiScope("CatalogReadPermission", "Read-only access to catalog operations"),
+        new ApiScope("DiscountFullPermission", "Full access to discount operations"),
+        new ApiScope("OrderFullPermission", "Full access to order operations")
+    };
+
+    public static IEnumerable<Client> Clients => new Client[]
+    {
+        // Visitor
+        new Client
+        {
+            ClientId = "MultiShopVisitorId",
+            ClientName = "Multi Shop Visitor User",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("multishopvisitorsecret".Sha256()) },
+            AllowedScopes = { "CatalogReadPermission" }
+        },
+
+        // Manager
+        new Client
+        {
+            ClientId = "MultiShopManagerId",
+            ClientName = "Multi Shop Manager User",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("multishopmanagersecret".Sha256()) },
+            AllowedScopes = { "CatalogFullPermission" }
+        },
+
+        // Admin
+        new Client
+        {
+            ClientId = "MultiShopAdminId",
+            ClientName = "Multi Shop Admin User",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("multishopadminsecret".Sha256()) },
+            AllowedScopes = 
+            { 
+                "CatalogFullPermission", 
+                "DiscountFullPermission", 
+                "OrderFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
             },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
-        };
+            AccessTokenLifetime = 600
+        }
+    };
 }
