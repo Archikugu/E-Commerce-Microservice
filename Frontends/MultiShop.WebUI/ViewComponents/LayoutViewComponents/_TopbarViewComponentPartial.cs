506cb4c8e8cf23ae3ba85c4679c3d81aecf19b02
Archikugu <engokhangok@gmail.com>
2025-06-19 14:15:08 +0300

    Added Multi Shop UI template; separated layout into partial views and view components for modular structure

---
+++ Frontends/MultiShop.WebUI/ViewComponents/LayoutViewComponents/_TopbarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.LayoutViewComponents
+{
+    public class _TopbarViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
